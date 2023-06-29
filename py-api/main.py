06ca664065074a5b51c0912f5e0294b869d76e42
Tim C. Steinmetz <tim@hakio.com>
2023-06-29 11:23:06 +0200

    Added Auth0/semi-generic JWT authentication and authorization

--- py-api/main.py
+++ py-api/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, Request
+from fastapi import FastAPI, Depends, Request, Response, status
 from fastapi.security import HTTPBearer
 from fastapi.middleware.cors import CORSMiddleware
 from jose import jwt
@@ -6,6 +6,10 @@ from functools import wraps
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
 import os
+from .utils import VerifyToken, jwt_secured
+import requests
+import json
+
 
 app = FastAPI()
 app.add_middleware(
@@ -15,61 +19,16 @@ app.add_middleware(
     allow_methods=["*"],  # Set the appropriate HTTP methods
     allow_headers=["*"],  # Set the appropriate HTTP headers
 )
-security = HTTPBearer()
+token_auth_scheme = HTTPBearer()
+
 
 # Load environment variables from .env file
 load_dotenv()
-
-# Auth0 configuration
 AUTH0_DOMAIN = os.getenv('AUTH0_DOMAIN')
-AUTH0_CLIENT_ID = os.getenv('AUTH0_CLIENT_ID')
+AUTH0_API_AUDIENCE = os.getenv('AUTH0_API_AUDIENCE')
+AUTH0_ALGORITHMS = os.getenv('AUTH0_ALGORITHMS')
+AUTH0_ISSUER = os.getenv('AUTH0_ISSUER')
 
-API_IDENTIFIER = os.getenv('API_IDENTIFIER')
-PUB_KEY = os.getenv('PUB_KEY')
-ALGORITHMS = ['RS256']
-TOKEN_EXPIRATION = int(os.getenv('TOKEN_EXPIRATION', 3600))  # Default expiration time of 1 hour
-
-# Auth0 decorator
-def auth0_secured():
-    def decorator(func):
-        @wraps(func)
-        async def wrapper(request: Request, *args, **kwargs):
-            authorization: str = request.headers.get("Authorization")
-            if not authorization or not authorization.startswith("Bearer "):
-                return {"message": "Invalid authorization header"}, 401
-
-            token = authorization.split(" ")[1]
-            try:
-                payload = jwt.decode(
-                    token,
-                    PUB_KEY,
-                    algorithms=ALGORITHMS,
-                    audience=API_IDENTIFIER,
-                    issuer=f'https://{AUTH0_DOMAIN}/',
-                )
-
-                print(payload)
-                # Perform additional checks or extract information from the payload if needed
-                # ...
-            except Exception as e:
-                return {"message": f"Authentication failed {e}"}, 401
-
-            return await func(request, *args, **kwargs)
-        return wrapper
-    return decorator
-
-
-# Protected route
-@app.get("/protected")
-@auth0_secured()
-async def protected_route(request: Request):
-    return {"message": "This route is protected"}
-
-
-@app.get("/secret")
-@auth0_secured()
-async def protected_route(request: Request):
-    return {"message": "This route is protected"}
 
 
 # Public route
@@ -78,69 +37,35 @@ async def public_route():
     return {"secret": "Mega secret message"}
 
 
-# Token validation endpoint
-@app.get("/validate_token")
-async def validate_token(access_token: str):
-    try:
-        payload = jwt.decode(
-            access_token,
-            PUB_KEY,
-            algorithms=ALGORITHMS,
-            audience=API_IDENTIFIER,
-            issuer=f'https://{AUTH0_DOMAIN}/',
-        )
-        # Perform additional checks or extract information from the payload if needed
-        # ...
-
-        return {"message": "Token validation successful"}
-    except Exception as e:
-        return {"message": "Token validation failed"}
+@app.get("/secret")
+#@auth0_secured()
+async def protected_route(token: str = Depends(token_auth_scheme)):
+    result = VerifyToken(token.credentials).verify()
+    if result.get("status"):
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return result
+ 
+    return {"message": "Secret stuffz"}
 
 
-# Token refresh endpoint
-@app.get("/refresh_token")
-async def refresh_token(refresh_token: str):
-    try:
-        payload = jwt.decode(
-            refresh_token,
-            PUB_KEY,
-            algorithms=ALGORITHMS,
-            audience=AUTH0_CLIENT_ID,
-            issuer=f'https://{AUTH0_DOMAIN}/',
-        )
-        # Perform additional checks or extract information from the payload if needed
-        # ...
-
-        # Generate new access token with an updated expiration time
-        new_payload = {
-            "sub": payload["sub"],
-            "exp": datetime.utcnow() + timedelta(seconds=TOKEN_EXPIRATION),
-            "aud": payload["aud"],
-            "iss": payload["iss"],
-            # Add additional claims as needed
-            # ...
-        }
-        new_access_token = jwt.encode(new_payload, PRIVATE_KEY, algorithm=ALGORITHMS[0])
-
-        return {"access_token": new_access_token}
-    except Exception as e:
-        return {"message": "Token refresh failed"}
+# Protected route
+@app.get("/more-secrets")
+@jwt_secured()
+async def deco_protected_route(request: Request, token: str = Depends(token_auth_scheme)):
+    return {"message": "This route is decorator protected"}
 
 
 # Token refresh endpoint
-@app.get("/get_jwt")
-async def get_jwt(access_token: str):
+@app.post("/get_jwt")
+async def get_jwt(request: Request):
+    post_data = await request.json()
     try:
-        import requests
-        import json
-
         # Define the request payload
         payload = {
-            "access_token": access_token,
-            "client_id": "ARWY5KrA22904NEPtrz9xyEU5h5xx6zs",
-            "client_secret": "aavSGUgWPq-_Y3Rq1xI59Vx6zjB-VsgTo5J7pugPfEGjDkp9WBJB1UmerU1av3am",
-            "audience": "http://localhost:8000/",
-            "grant_type": "client_credentials"
+            "client_id": post_data['client_id'],
+            "client_secret": post_data['client_secret'],
+            "audience": post_data['audience'],
+            "grant_type": post_data['grant_type']
         }
 
         # Set the headers
@@ -149,36 +74,16 @@ async def get_jwt(access_token: str):
         }
 
         # Send the POST request
-        response = requests.post("https://dev-po2xt7vso71m0yqb.eu.auth0.com/oauth/token", json=payload, headers=headers)
+        response = requests.post(f"https://{AUTH0_DOMAIN}/oauth/token", json=payload, headers=headers)
 
         # Check if the request was successful (status code 200)
         if response.status_code == 200:
             # Extract the access token from the response
             bearer_token = response.json()["access_token"]
-            print("Access Token:", access_token)
+            #print("Access Token:", access_token)
         else:
             print("Request failed with status code:", response.status_code)
     
         return {"bearer_token": bearer_token}
     except Exception as e:
         return {"message": f"Token refresh failed {e}"}
-
-
-@app.get("/get_auth0_token")
-async def get_auth0_token():
-    from auth0.authentication import GetToken
-
-    try:
-
-        domain = 'dev-po2xt7vso71m0yqb.eu.auth0.com'
-        non_interactive_client_id = 'xxn934YhU4OR6ji76regOns75JMrzxIZ'
-        non_interactive_client_secret = 'GgMxErM0LbpH31NLNCkZfy5Wa7ylcHSpaBShcxi5_HYkEcQjZRKNHPQMjnJkVfCT'
-
-        get_token = GetToken(domain, non_interactive_client_id, client_secret=non_interactive_client_secret)
-        token = get_token.client_credentials(f'https://{domain}/oauth/token/')
-        mgmt_api_token = token['access_token']
-
-        print(mgmt_api_token)
-        
-    except Exception as e:
-        print(e)
---
+++ py-api/utils.py
@@ -0,0 +1,73 @@
+
+import os
+from fastapi import Depends, Request
+from fastapi.security import HTTPBearer
+import jwt
+from functools import wraps
+
+token_auth_scheme = HTTPBearer()
+
+
+def set_up_auth0():
+    """Sets up configuration for the app"""
+    config = {
+        "DOMAIN": os.getenv("AUTH0_DOMAIN", "your.domain.com"),
+        "API_AUDIENCE": os.getenv("AUTH0_API_AUDIENCE", "your.audience.com"),
+        "ISSUER": os.getenv("AUTH0_ISSUER", "https://your.domain.com/"),
+        "ALGORITHMS": os.getenv("AUTH0_ALGORITHMS", "RS256"),
+    }
+    return config
+
+
+class VerifyToken():
+    """Does all the token verification using PyJWT"""
+
+    def __init__(self, token):
+        self.token = token
+        self.config = set_up_auth0()
+
+        # This gets the JWKS from a given URL and does processing so you can
+        # use any of the keys available
+        jwks_url = f'https://{self.config["DOMAIN"]}/.well-known/jwks.json'
+        self.jwks_client = jwt.PyJWKClient(jwks_url)
+
+    def verify(self):
+        # This gets the 'kid' from the passed token
+        try:
+            self.signing_key = self.jwks_client.get_signing_key_from_jwt(
+                self.token
+            ).key
+        except jwt.exceptions.PyJWKClientError as error:
+            return {"status": "error", "msg": error.__str__()}
+        except jwt.exceptions.DecodeError as error:
+            return {"status": "error", "msg": error.__str__()}
+
+        try:
+            payload = jwt.decode(
+                self.token,
+                self.signing_key,
+                algorithms=self.config["ALGORITHMS"],
+                audience=self.config["API_AUDIENCE"],
+                issuer=self.config["ISSUER"],
+            )
+        except Exception as e:
+            return {"status": "error", "message": str(e)}
+
+        return payload
+
+
+# Auth0 decorator
+def jwt_secured():
+    def decorator(func):
+        @wraps(func)
+        async def wrapper(request: Request, token: str = Depends(token_auth_scheme), *args, **kwargs):
+            try:
+                result = VerifyToken(token.credentials).verify()
+            except Exception as e:
+                return {"message": f"Authentication failed {e}"}, 401
+
+            return await func(request, token, *args, **kwargs)
+
+        return wrapper
+
+    return decorator
